ab35f0599144abc3ca482629df3d25431a22a925
kianalmeyda <xynnmain@gmail.com>
2026-01-19 04:49:21 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -5,4 +5,5 @@ PRODUCT_SHIPPING_API_LEVEL := 31
 
 # Recovery
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/recovery.fstab:$(TARGET_COPY_OUT_RECOVERY)/root/system/etc/recovery.fstab
+    $(LOCAL_PATH)/recovery.fstab:$(TARGET_COPY_OUT_RECOVERY)/root/system/etc/recovery.fstab \
+    $(LOCAL_PATH)/prebuilt/dtb.img:$(OUT)/dtb.img

d349670d935614b1273904d84f78873cab3b7ca2
kianalmeyda <xynnmain@gmail.com>
2026-01-19 01:56:52 +0800

    Update twrp_X6885.mk

--- twrp_X6885.mk
+++ twrp_X6885.mk
@@ -2,7 +2,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from OrangeFox Common
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Device identifier
 PRODUCT_DEVICE := X6885

7de16c6adf98d7df4ce4be0b2c8fbd9db0871da5
kianalmeyda <xynnmain@gmail.com>
2026-01-19 05:05:53 +0800

    ff
    ff

---
+++ Android.mk
@@ -0,0 +1,8 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := dtb.img
+LOCAL_SRC_FILES := prebuilt/dtb.img
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(PRODUCT_OUT)
+include $(BUILD_PREBUILT)

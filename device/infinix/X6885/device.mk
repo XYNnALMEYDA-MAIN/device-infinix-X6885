7b451588c05c0b42c9a75c6d9f4e19f62153d17d
kianalmeyda <xynnmain@gmail.com>
2026-01-19 01:32:56 +0800

    fg
    gf

--- device/infinix/X6885/device.mk
+++
@@ -1,8 +0,0 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
-# API
-PRODUCT_SHIPPING_API_LEVEL := 31
-
-# Recovery
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/recovery.fstab:$(TARGET_COPY_OUT_RECOVERY)/root/system/etc/recovery.fstab

fa606dc8f91c2f784fc43704bc046c2c0bebcdb6
kianalmeyda <xynnmain@gmail.com>
2026-01-19 01:01:59 +0800

    y
    y

---
+++ git_repo/device.mk
@@ -0,0 +1,8 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# API
+PRODUCT_SHIPPING_API_LEVEL := 31
+
+# Recovery
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/recovery.fstab:$(TARGET_COPY_OUT_RECOVERY)/root/system/etc/recovery.fstab
